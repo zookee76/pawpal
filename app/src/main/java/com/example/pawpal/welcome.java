82762bef7b81f7895c2432b0d4e13992b2a41f03
zookee76 <135536455+zookee76@users.noreply.github.com>
2024-10-13 21:51:58 +0800

    almost done

--- app/src/main/java/com/example/pawpal/welcome.java
+++ app/src/main/java/com/example/pawpal/welcome.java
@@ -1,6 +1,11 @@
 package com.example.pawpal;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +16,15 @@ public class welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_welcome);
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(welcome.this, welcomeaccess.class);
+                startActivity(intent);
+            }
+        });
     }
 }
