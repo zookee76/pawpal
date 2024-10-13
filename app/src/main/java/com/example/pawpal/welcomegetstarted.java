322d1a22f40d48bd6bf65a7696c77f2c34e68166
zookee76 <135536455+zookee76@users.noreply.github.com>
2024-10-13 23:20:14 +0800

    done with welcome also added photos

--- app/src/main/java/com/example/pawpal/welcomegetstarted.java
+++ app/src/main/java/com/example/pawpal/welcomegetstarted.java
@@ -1,6 +1,9 @@
 package com.example.pawpal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +12,15 @@ public class welcomegetstarted extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.welcome_getstarted);
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(welcomegetstarted.this, welcomerole.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
---
+++ app/src/main/java/com/example/pawpal/welcomerole.java
@@ -0,0 +1,16 @@
+package com.example.pawpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class welcomerole extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.welcome_role);
+    }
+}
