ac935c5f0580a39160a7ab0f4303fce4857de361
ashley <nicole_corpuz@dlsu.edu.ph>
2024-10-23 20:21:49 +0800

    all new changes are now reflected (for interactions)

--- app/src/main/java/com/example/pawpal/pforgotpw.java
+++ app/src/main/java/com/example/pawpal/pforgotpw.java
@@ -1,6 +1,9 @@
 package com.example.pawpal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,15 @@ public class pforgotpw extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_forgotpw);
+
+        Button newpw_btn = findViewById(R.id.btn_newpw);
+
+        newpw_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(pforgotpw.this, plogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
