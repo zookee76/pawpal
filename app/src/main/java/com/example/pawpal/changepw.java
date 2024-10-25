7dcf661ed379d1f114d22cc1bba2dc722309442b
herisevisto <herise_visto@dlsu.edu.ph>
2024-10-25 16:42:12 +0800

    changepw updated!

--- app/src/main/java/com/example/pawpal/changepw.java
+++ app/src/main/java/com/example/pawpal/changepw.java
@@ -1,6 +1,9 @@
 package com.example.pawpal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,15 @@ public class changepw extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_changepw);
+
+        Button savepw = findViewById(R.id.btn_savepw);
+
+        savepw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(changepw.this, userprofilepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
